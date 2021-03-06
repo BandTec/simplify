2c9a70866e325914dc8353c7f95ed0570d4460db
carinadidomenico <carinadidomenico@gmail.com>
2021-03-06 18:50:59 -0300

    Aterações de texto e layout site institucional

--- 4-Semestre/App/Aplicação Web/simplify-angular/src/app/shared/components/navbar/navbar.component.ts
+++ 4-Semestre/App/Aplicação Web/simplify-angular/src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  toggleMenu:Boolean = false
+    
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
+  showMenu() {
+    return this.toggleMenu = !this.toggleMenu;
+    
+  }
+  
 }
