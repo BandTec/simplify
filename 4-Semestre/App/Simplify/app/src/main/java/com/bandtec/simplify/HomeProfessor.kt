0e138e3c7aa7eceb2276fb9642d210861756b8d3
unknown <pedro.goncalves@bandtec.com.br>
2021-03-17 02:01:15 -0300

    Criação da tela inicial de professor

---
+++ 4-Semestre/App/Simplify/app/src/main/java/com/bandtec/simplify/HomeProfessor.kt
@@ -0,0 +1,14 @@
+package com.bandtec.simplify
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeProfessor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_estudante)
+
+    }
+}
